6066a52a1f7ea895a36baf73091981c2c4539584
root <peterstephen22000@gmail.com>
2022-09-10 00:00:13 -0700

    prints all the numbers of base 16 in lowercase, followed by a new line

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
-
-
-
 /**
-*
-*main - prints hexadecimal base 0123456789abcdef, using putchar
-*
+*main- prints hexadecimal base 0123456789abcdef, using putchar
 *Return: Always 0 (Success)
 */
-
-
-
 int main(void)
 
 {
-
 	int n = '0';
 
 	int a_to_f = 'a';
