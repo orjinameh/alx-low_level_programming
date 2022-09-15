f2bf61350bf1ba0809e08b7bc720708339e2ed50
root <peterstephen22000@gmail.com>
2022-09-15 10:11:14 -0700

    initial commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _isupper(int c);
+#include "main.h"
+
 /**
 * _isdigit - checks for a digit 0 through 9
 * @c: digit to be checked
 * Return: 1 if true, 0 if false
 */
 
-int _isdigit(int c);
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
 
-#endif
+	return (0);
+}
