291c519659cb5480f1f99ab2074341a45fd04900
root <peterstephen22000@gmail.com>
2022-09-15 14:34:10 -0700

    initial commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _isupper(int c);
-/**
-* _isdigit - checks for a digit 0 through 9
-* @c: digit to be checked
-* Return: 1 if true, 0 if false
-*/
 
 int _isdigit(int c);
 
