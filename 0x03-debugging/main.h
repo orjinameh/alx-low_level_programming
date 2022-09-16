6dff660bc21fc983118eb824ca6e2ecc1d23dae5
root <peterstephen22000@gmail.com>
2022-09-15 21:30:22 -0700

    initial commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int largest_number(int a, int b, int c);
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
 
-int largest_number(int a, int b, int c);
-
-#endif /* MAIN_H */
+#endif
