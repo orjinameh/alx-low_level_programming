aaa12f46c2578fcd9450f18edc7eb4f2ac20bdd2
root <peterstephen22000@gmail.com>
2022-09-21 14:50:47 -0700

    initial commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,36 +5,64 @@
 *Return: returns 0 always
 */
 
-int main(void)
+int main()
+
 {
-	int units;
-	int tens;
-	int hundreds;
-	int thou;
 
-	for (thou = '0'; thou <= '9'; thou++)
-	{
-		for (hundreds = '0'; hundreds <= '9'; hundreds++)
-		{
-			for (tens = thou; tens <= '9'; tens++)
-			{
-				for (units = (hundreds + 1); units <= '9'; units++)
-				{
-					putchar(thou);
-					putchar(hundreds);
-					putchar(' ');
-					putchar(tens);
-					putchar(units);
+    char alpha;
 
-					if (!((thou == '9' && hundreds == '8') && (tens == '9' && units == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
-	}
+    char alpha2;
 
-	return (0);
+    char alpha3;
+
+    char alpha4;
+
+    for (alpha4 = '0'; alpha4 <= '9'; alpha4++)
+
+    {
+
+        for(alpha3 = '0'; alpha3 <= '9'; alpha3++)
+
+        {
+
+            for(alpha2 = alpha4; alpha2 <= '9'; alpha2++)
+
+            {
+
+                for(alpha= (alpha3 + 1); alpha <= '9'; alpha++)
+
+                {
+
+                    putchar(alpha4);
+
+                    putchar(alpha3);
+
+                    putchar(' ');
+
+                    putchar(alpha2);
+
+                    putchar(alpha);
+
+                    if(alpha4 == '9' && alpha3 == '8')
+
+                    {
+
+                        break;
+
+                    }
+
+                    putchar(',');
+
+                    putchar(' ');
+
+                }
+
+            }
+
+        }
+
+    }
+
+    putchar('\n');
+
 }
