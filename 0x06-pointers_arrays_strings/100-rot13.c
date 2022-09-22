7860c4e5c9883007300886781675761b9093423b
root <peterstephen22000@gmail.com>
2022-09-21 22:51:16 -0700

    initial commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,8 +14,7 @@ char *rot13(char *s)
 		while ((s[i] >= 'a' && s[i] <= 'z') ||
 				(s[i] >= 'A' && s[i] <= 'Z'))
 		{
-			if ((s[i] >= 'a' && s[i] <= 'm') ||
-					(s[i] >= 'A' && s[i] <= 'M'))
+			if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
 				s[i] += 13;
 			else
 				s[i] -= 13;
