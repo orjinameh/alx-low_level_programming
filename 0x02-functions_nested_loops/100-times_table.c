a7567c1721d1a91a6410cc20f90070216c800f88
root <peterstephen22000@gmail.com>
2022-09-23 23:22:58 -0700

    initial commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,10 +12,11 @@ void print_times_table(int n)
 
 	if (n < 16 && n >= 0)
 	{
+		inc = '0';
+
 		for (x_axis = n; x_axis >= 0; x_axis--)
 		{
 			pn = '0';
-			inc = 0;
 
 			for (y_axis = n; y_axis >= 0; y_axis--)
 			{
