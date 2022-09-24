bcea9a4ca43d260bfcf39f9fd6b08355ca1abb8d
root <peterstephen22000@gmail.com>
2022-09-23 23:56:05 -0700

    initial commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,8 +7,7 @@
 
 void print_times_table(int n)
 {
-	int x_axis, y_axis, inc;
-	char pn;
+	int x_axis, y_axis, inc, pn;
 
 	if (n < 16 && n >= 0)
 	{
@@ -16,12 +15,12 @@ void print_times_table(int n)
 
 		for (x_axis = n; x_axis >= 0; x_axis--)
 		{
-			pn = '0';
+			pn = 0;
 
 			for (y_axis = n; y_axis >= 0; y_axis--)
 			{
-				if (pn <= '9')
-					_putchar(pn);
+				if (pn <= 9)
+					_putchar(pn + '0');
 				if (y_axis > 0)
 				{
 					_putchar(',');
@@ -30,6 +29,11 @@ void print_times_table(int n)
 						_putchar(' ');
 					if (y_axis <= 99)
 						_putchar(' ');
+				}
+				if (pn > 9 && pn <= 99)
+				{
+					_putchar((pn / 10) + '0');
+					_putchar((pn % 10) + '0')
 				}
 				pn += inc;
 			}
