3cf85e835a291e097936d1b17cad89528c188242
root <peterstephen22000@gmail.com>
2022-09-26 14:59:11 -0700

    copying memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -24,13 +24,13 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	int i;
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++, src++)
+		for (i = 0; i < n; i++)
 		{
-			dest[i] = *src;
+			dest[i] = src[i];
 		}
 	}
 
